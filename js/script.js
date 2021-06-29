431f8a4b98220c448bf8353d79ed0697ef7bfa14
simonedimeglio <simonedimeglio@icloud.com>
2021-06-29 15:14:01 +0200

    Methods on

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ Vue.config.devtools = true;
 
 new Vue(
     {
-        el: '#app',
+        el: '#app', // id "app" in html
         
-        data: { 
+        // le mie variabili
+        data: {
             toDoList: [ // Array contenente le tasks
                 'Comprare il pane',
                 'Buttare immondizia',
@@ -13,12 +14,34 @@ new Vue(
             ],
             newTask: '', // inizializzo per l'input la stringa vuota
         },
+
         // * Per il focus automatico su input in html
         mounted() {
 
         },
+
+        // le mie funzioni
         methods: {
-    
+
+            // funzione per l'autofocus 
+            autofocus: function() {
+                const element = document.getElementById('newTaskItem'); // seleziono l'elemento in html
+                element.focus(); // gli assegno il focus
+            },
+
+            // funzione per aggiungere una task all'elenco (array toDoList)
+            addTask: function() {
+                this.toDoList.push(this.newTask); // prendo il valore scritto e lo pusho nell'array
+                this.newTask = ''; // lo inizializzo nuovamente per permettere di scrivere il nuovo task
+                this.autofocus(); // assegno il focus all'input
+            },
+
+            // funzione per eliminare una task dall'elenco (array toDoList)
+            deleteTask: function(index) {
+                this.toDoList.splice(index, 1); // taglio un elemento a partire dall'index dell'elemento selezionato
+                                                // in pratica elimino solo ed esclusivamente quello selezionato
+                this.autofocus(); // assegno il focus all'input
+            }
         }    
     }
 );
