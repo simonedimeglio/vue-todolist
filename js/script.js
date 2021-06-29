8b93472a76e50377e990cec9563957a624aaa2b5
simonedimeglio <simonedimeglio@icloud.com>
2021-06-29 15:49:25 +0200

    BONUS: clear button on

--- js/script.js
+++ js/script.js
@@ -31,15 +31,26 @@ new Vue(
 
             // funzione per aggiungere una task all'elenco (array toDoList)
             addTask: function() {
-                this.toDoList.push(this.newTask); // prendo il valore scritto e lo pusho nell'array
-                this.newTask = ''; // lo inizializzo nuovamente per permettere di scrivere il nuovo task
-                this.autofocus(); // assegno il focus all'input
+                if (this.newTask === '') {
+                    alert('Per aggiungere un elemento alla lista, devi prima scriverlo');
+                    this.autofocus(); // assegno il focus all'input
+                } else {
+                    this.toDoList.push(this.newTask); // prendo il valore scritto e lo pusho nell'array
+                    this.newTask = ''; // lo inizializzo nuovamente per permettere di scrivere il nuovo task
+                    this.autofocus(); // assegno il focus all'input
+                }
             },
 
             // funzione per eliminare una task dall'elenco (array toDoList)
             deleteTask: function(index) {
                 this.toDoList.splice(index, 1); // taglio un elemento a partire dall'index dell'elemento selezionato
                                                 // in pratica elimino solo ed esclusivamente quello selezionato
+                this.autofocus(); // assegno il focus all'input
+            },
+
+            // funzione per azzerare ("pulire") la lista (eliminare ogni elemento dell'array)
+            deleteList: function() {
+                this.toDoList = []; // inizializzo l'array "svuotandolo"
                 this.autofocus(); // assegno il focus all'input
             }
         }    
